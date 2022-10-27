54501d13dc83d15d776c0384b7d8d7c77972c26a
Agustin Bordachar <agustinbordachar11@gmail.com>
2022-10-27 19:12:34 -0300

    segundo commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,28 @@
-import { Box } from '@chakra-ui/react';
-import React, { useEffect } from 'react';
+import { Box, propNames, useComponentStyles__unstable } from '@chakra-ui/react';
+import { map } from 'leaflet';
 
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
-const MapreRender = ({ location }) => {
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useMemo,
+  useCallback,
+} from 'react';
+
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  Circle,
+  useMapEvent,
+} from 'react-leaflet';
+
+// Dragable marker
+
+// end
+const MapreRender = ({ location, test }) => {
   const map = useMap();
 
   useEffect(() => {
@@ -12,6 +32,44 @@ const MapreRender = ({ location }) => {
     );
   }, [location]);
   return;
+};
+
+const DraggableMarker = ({ location }) => {
+  const [draggable, setDraggable] = useState(true);
+  const [position, setPosition] = useState({
+    lat: location.latitude,
+    lng: location.longitude,
+  });
+  const map = useMap();
+
+  useEffect(() => {
+    map.setView({ lat: position.lat, lng: position.lng }, map.getZoom());
+  }, [position]);
+
+  const markerRef = useRef(null);
+
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker != null) {
+          setPosition(marker.getLatLng());
+        }
+      },
+    }),
+    []
+  );
+
+  return (
+    <Marker
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+    >
+      <Popup minWidth={90}>You are here!</Popup>
+    </Marker>
+  );
 };
 
 const Map = ({ location }) => {
@@ -26,18 +84,11 @@ const Map = ({ location }) => {
       style={{ height: '90vh', width: '100wh' }}
     >
       <MapreRender location={location} />
+      <DraggableMarker location={location} />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker
-        position={{
-          lng: location.longitude,
-          lat: location.latitude,
-        }}
-      >
-        <Popup>You are here!</Popup>
-      </Marker>
     </MapContainer>
   );
 };
